74526ccde0d57422ffb809695d8c8686851b85cc
Linus Styrén <linus.styren@aptiv.com>
2020-11-24 09:09:29 +0100

    use composition api

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
+import VueCompositionAPI from "@vue/composition-api";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
+Vue.use(VueCompositionAPI);
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
